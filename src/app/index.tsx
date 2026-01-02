55138f48afad231bcc1ec44b4795b25a929b084b
outa1127 <ota.ono@livesense.co.jp>
2026-01-02 16:52:56 +0900

    tamaguiのインストール

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,15 +1,20 @@
+import { Button, createTamagui,Input,TamaguiProvider, View } from 'tamagui'
+import { defaultConfig } from '@tamagui/config/v4'
 import { Link } from "expo-router"
-import { Text, View } from "react-native"
+import { Text } from "react-native"
+
+const config = createTamagui(defaultConfig)
 
 const Index = () => {
   return (
-    <View>
+    <TamaguiProvider config={config}>
       <Text style={{ fontSize: 20, fontWeight: "bold" }}>ブログアプリ</Text>
       <View>
         <Link href="/blog">ブログ一覧へ</Link>
         <Link href="/blog/create">ブログ作成へ</Link>
       </View>
-    </View>
+      <Button size="$3" color="$blue10">ボタン</Button>
+    </TamaguiProvider>
   )
 }
 
