8adf8411966c08546f72505912591a73b86289c4
outa1127 <ota.ono@livesense.co.jp>
2026-01-02 14:53:13 +0900

    layout の編集

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,7 +1,19 @@
 import { Slot } from "expo-router"
+import { StyleSheet, View } from "react-native"
 
 const Layout = () => {
-  return <Slot />
+  return (
+    <View style={styles.container}>
+      <Slot />
+    </View>
+  )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    marginTop: 50
+  }
+})
 
 export default Layout
